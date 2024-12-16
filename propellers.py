51fbd6bf8d2d730b3dad69b09885ac6b7cd6fd90
h4cker3 <vladimir05112007@gmail.com>
2024-12-16 11:04:56 +0300

    Added a class of propellers (without forward method).

---
+++ propellers.py
@@ -0,0 +1,56 @@
+import torch
+
+
+class Propellers():
+    """
+    Class of n propellers. Operates on torch.tensors and uses physics simulation of real-life propellers.
+
+    Attributes:
+        n (int): Number of propellers, operating in one class.
+        dia (torch.tensor): Vector of shape (n), containing torch.float values of diameters (meter).
+        pitch (torch.tensor): Vector of shape (n), containing torch.float values of pitchs (meter).
+        speed (torch.tensor): Vector of shape (n), containing torch.float values of speeds (rotations/min).
+        thrust (torch.tensor): Vector of shape (n), containing torch.float values of thrusts (kilogram).
+    """
+    def __init__(self, n_propellers: int, propeller_diameters: list, propeller_pitchs: list):
+        """
+        Initilizes a class of n propellers.
+
+        Attributes:
+            n_propellers (int): Number of propellers, operating in one class.
+
+            propeller_diameters (list): Vector of shape (n_propellers), containing torch.float values of diameters (meter).
+
+            propeller_pitch (list): Vector of shape (n_propellers), containing torch.float values of pitchs (meter).
+        """
+        self.n = n_propellers
+        if len(propeller_diameters) != n_propellers:
+            raise ValueError(f"Shape of propeller_diameters ({len(propeller_diameters)}) not equal to n_propellers ({n_propellers}).")
+        self.dia = torch.tensor(propeller_diameters) * 39.37 # to inches
+        if len(propeller_pitchs) != n_propellers:
+            raise ValueError(f"Shape of propeller_pitchs ({len(propeller_pitchs)}) not equal to n_propellers ({n_propellers}).")
+        self.pitch = propeller_pitchs * 39.37 # to inches
+        self.speed = torch.zeros((self.n))
+        self.thrust = torch.zeros((self.n))
+    
+
+
+    def set_speed(self, propeller_speeds: torch.tensor):
+        """
+        Sets a speeds for propellers in class. Automaticly counts a trust for every propeller, according on speed.
+
+        Attributes:
+            speed (torch.tensor): Vector of shape (n), containing speed torch.float values for propellers (rotations/min).
+        """
+        if propeller_speeds.shape != [self.n]:
+            raise ValueError("Shape of speed not equal to [n_propellers].")
+        self.speed = propeller_speeds
+        self.thrust = 4.392e-8 * self.speed * torch.pow(self.dia,3.5)/(torch.sqrt(self.pitch))
+        self.thrust = 0.101972*self.thrust*(4.23e-4 * self.speed * self.pitch)
+    
+    # TODO: Add a forward alias for set_speed
+
+
+# example of work:
+# prop = Propellers(4, [3, 3, 3, 3], [2, 2, 2, 2])
+# prop.set_speed(torch.tensor([3, 2, 1, 7]))
